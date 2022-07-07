fb7d6e76ee800b9d0255bb7194f95ea9faaeb663
sharapov <sharapov@SHARAPOV-AL>
2022-07-07 09:55:33 +0300

    Добавить проект с примером - Построение конвеера обработки запроса
     - пример добавлен

---
+++ HelloWebApp/1_16_Построение конвейера обработки запроса/ErrorHandlingMiddleware.cs
@@ -0,0 +1,26 @@
+﻿namespace _1_16_Построение_конвейера_обработки_запроса
+{
+    public class ErrorHandlingMiddleware
+    {
+        readonly RequestDelegate next;
+        public ErrorHandlingMiddleware(RequestDelegate next)
+        {
+            this.next = next;
+        }
+        //В отличие от предыдущих двух компонентов ErrorHandlingMiddleware сначала передает запрос на выполнение последующим делегатам, а потом уже сам обрабатывает.
+        //Это возможно, поскольку каждый компонент обрабатывает запрос два раза: вначале вызывается та часть кода, которая идет до await next.Invoke(context);,
+        //а после завершения обработки последующих компонентов вызывается та часть кода, которая идет после await next.Invoke(context);.
+        public async Task InvokeAsync(HttpContext context)
+        {
+            await next.Invoke(context);
+            if (context.Response.StatusCode == 403)
+            {
+                await context.Response.WriteAsync("Access Denied");
+            }
+            else if (context.Response.StatusCode == 404)
+            {
+                await context.Response.WriteAsync("Not Found");
+            }
+        }
+    }
+}
---
+++ HelloWebApp/1_16_Построение конвейера обработки запроса/RoutingMiddleware.cs
@@ -0,0 +1,29 @@
+﻿namespace _1_16_Построение_конвейера_обработки_запроса
+{
+    //Этот компонент в зависимости от строки запроса возвращает либо определенную строку, либо устанавливает код ошибки.
+    public class RoutingMiddleware
+    {
+        readonly RequestDelegate next;
+        public RoutingMiddleware(RequestDelegate next)
+        {
+            this.next = next;
+        }
+        public async Task InvokeAsync(HttpContext context)
+        {
+            string path = context.Request.Path;
+            if (path == "/index")
+            {
+                await context.Response.WriteAsync("Home Page");
+            }
+            else if (path == "/about")
+            {
+                await context.Response.WriteAsync("About Page");
+            }
+            else
+            {
+                context.Response.StatusCode = 404;
+                await context.Response.WriteAsync("Not Found");
+            }
+        }
+    }
+}
